57c38274f0236b7a0e07f6d153be753bf95feb36
Vincent Alsteen <vincent.alsteen@gmail.com>
2020-12-17 09:40:13 +0100

    isolate canvas-built ui control

--- app/src/main/java/com/djcrontab/code/controlsurface/App.kt
+++ app/src/main/java/com/djcrontab/code/controlsurface/App.kt
@@ -31,15 +31,10 @@ import kotlin.math.sin
 
 @Composable
 fun Encoder(
-    controlState: ControllerState
+    controllerState: ControllerState
 ) {
-
-    val name by controlState.name.observeAsState()
-    val value by controlState.parameterValue.observeAsState()
-    val displayValue by controlState.displayValue.observeAsState()
-    val encoderColor = colorResource(id = R.color.encoder)
-    val encoderColorFill = colorResource(id = R.color.encoderfill)
-    val encoderColorFillOff = colorResource(id = R.color.encoderoff)
+    val name by controllerState.name.observeAsState()
+    val displayValue by controllerState.displayValue.observeAsState()
 
     Column {
         val textHeight = with(AmbientDensity.current) { 22.sp.toDp() }
@@ -57,126 +52,7 @@ fun Encoder(
         }
         Spacer(modifier = Modifier.preferredHeight(3.dp).weight(1.9f))
 
-        Box(Modifier.weight(6f).fillMaxSize()) {
-
-            WithConstraints(Modifier.fillMaxSize()) {
-                val boxWidth = constraints.maxWidth
-                Canvas(
-                    Modifier
-                        .doubleTapGestureFilter { controlState.focus() }
-                        .dragGestureFilter(
-                            dragObserver = object : DragObserver {
-                                override fun onDrag(dragDistance: Offset): Offset {
-
-                                    val relX = dragDistance.x / boxWidth.toFloat() / 4f
-                                    val relY = -dragDistance.y / boxWidth.toFloat() / 4f
-
-                                    controlState.onValueChanged(
-                                        ((value
-                                            ?: 0f) + relX + relY).coerceIn(0f, 1f)
-                                    )
-
-                                    return dragDistance
-                                }
-
-                                override fun onStart(downPosition: Offset) {
-                                    controlState.pauseRemoteUpdates = true
-                                }
-
-                                override fun onStop(velocity: Offset) {
-                                    controlState.pauseRemoteUpdates
-                                }
-                            }
-                        )
-                ) {
-
-                    val radius = size.width * 0.9f / 2f
-                    val topLeft = Offset(center.x - radius, center.y - radius)
-                    val calculatePhase = { value: Float, base: Float ->
-                        value * base / 2f - base / 2f
-                    }
-                    val phaseZero = calculatePhase(0f, 360f)
-                    val valuePhase = calculatePhase(value ?: 0f, 360f)
-                    val valuePhaseRadians = calculatePhase(value ?: 0f, (PI * 2f).toFloat())
-
-                    translate(topLeft.x, topLeft.y + textHeight.toPx()) {
-                        drawArc(
-                            color = encoderColor,
-                            startAngle = 180f,
-                            sweepAngle = 180f,
-                            topLeft = Offset.Zero,
-                            useCenter = false,
-                            size = Size(radius * 2f, radius * 2f),
-                            style = Stroke(1.dp.toPx()),
-                        )
-
-                        // half circle
-                        drawArc(
-                            color = encoderColorFillOff,
-                            startAngle = 180f,
-                            sweepAngle = 180f,
-                            topLeft = Offset.Zero,
-                            useCenter = false,
-                            size = Size(radius * 2f, radius * 2f),
-                            style = Fill
-                        )
-
-                        // arc filler
-                        drawArc(
-                            color = encoderColorFill,
-                            startAngle = phaseZero,
-                            sweepAngle = valuePhase - phaseZero,
-                            topLeft = Offset.Zero,
-                            useCenter = false,
-                            size = Size(radius * 2f, radius * 2f),
-                        )
-
-                        // value outline
-                        drawArc(
-                            color = encoderColor,
-                            startAngle = phaseZero,
-                            sweepAngle = valuePhase - phaseZero,
-                            topLeft = Offset.Zero,
-                            useCenter = false,
-                            size = Size(radius * 2f, radius * 2f),
-                            style = Stroke(width = 2.dp.toPx()),
-                        )
-
-                        // triangle to fill from arc filler to center
-                        drawPath(Path().apply {
-                            moveTo(0f, radius)
-                            lineTo(radius, radius)
-                            lineTo(
-                                radius + cos(valuePhaseRadians) * radius,
-                                sin(valuePhaseRadians) * radius + radius
-                            )
-                            close()
-                        }, encoderColorFill)
-
-                        drawLine(
-                            strokeWidth = 2.dp.toPx(),
-                            start = Offset(0f, radius),
-                            end = Offset(radius, radius),
-                            color = encoderColor,
-                        )
-
-                        translate(radius, radius) {
-                            drawLine(
-                                strokeWidth = 2.dp.toPx(),
-                                cap = StrokeCap.Square,
-                                color = encoderColor,
-                                start = Offset.Zero,
-                                end = Offset(
-                                    (cos(valuePhaseRadians) * radius),
-                                    (sin(valuePhaseRadians) * radius)
-                                )
-                            )
-                        }
-                    }
-
-                }
-            }
-        }
+        Odometer(Modifier.weight(6f).fillMaxSize(), controllerState)
 
         Spacer(modifier = Modifier.preferredHeight(4.dp).weight(2f))
 
@@ -189,6 +65,137 @@ fun Encoder(
         )
 
         Spacer(modifier = Modifier.preferredHeight(2.dp).weight(1.5f))
+    }
+}
+
+
+@Composable
+fun Odometer(modifier: Modifier = Modifier, controllerState: ControllerState) {
+    val value by controllerState.parameterValue.observeAsState()
+    val encoderColor = colorResource(id = R.color.encoder)
+    val encoderColorFill = colorResource(id = R.color.encoderfill)
+    val encoderColorFillOff = colorResource(id = R.color.encoderoff)
+    val textHeight = with(AmbientDensity.current) { 22.sp.toDp() }
+
+    Box(modifier) {
+
+        WithConstraints(Modifier.fillMaxSize()) {
+            val boxWidth = constraints.maxWidth
+            Canvas(
+                Modifier
+                    .doubleTapGestureFilter { controllerState.focus() }
+                    .dragGestureFilter(
+                        dragObserver = object : DragObserver {
+                            override fun onDrag(dragDistance: Offset): Offset {
+
+                                val relX = dragDistance.x / boxWidth.toFloat() / 4f
+                                val relY = -dragDistance.y / boxWidth.toFloat() / 4f
+
+                                controllerState.onValueChanged(
+                                    ((value
+                                        ?: 0f) + relX + relY).coerceIn(0f, 1f)
+                                )
+
+                                return dragDistance
+                            }
+
+                            override fun onStart(downPosition: Offset) {
+                                controllerState.pauseRemoteUpdates = true
+                            }
+
+                            override fun onStop(velocity: Offset) {
+                                controllerState.pauseRemoteUpdates
+                            }
+                        }
+                    )
+            ) {
+
+                val radius = size.width * 0.9f / 2f
+                val topLeft = Offset(center.x - radius, center.y - radius)
+                val calculatePhase = { value: Float, base: Float ->
+                    value * base / 2f - base / 2f
+                }
+                val phaseZero = calculatePhase(0f, 360f)
+                val valuePhase = calculatePhase(value ?: 0f, 360f)
+                val valuePhaseRadians = calculatePhase(value ?: 0f, (PI * 2f).toFloat())
+
+                translate(topLeft.x, topLeft.y + textHeight.toPx()) {
+                    drawArc(
+                        color = encoderColor,
+                        startAngle = 180f,
+                        sweepAngle = 180f,
+                        topLeft = Offset.Zero,
+                        useCenter = false,
+                        size = Size(radius * 2f, radius * 2f),
+                        style = Stroke(1.dp.toPx()),
+                    )
+
+                    // half circle
+                    drawArc(
+                        color = encoderColorFillOff,
+                        startAngle = 180f,
+                        sweepAngle = 180f,
+                        topLeft = Offset.Zero,
+                        useCenter = false,
+                        size = Size(radius * 2f, radius * 2f),
+                        style = Fill
+                    )
+
+                    // arc filler
+                    drawArc(
+                        color = encoderColorFill,
+                        startAngle = phaseZero,
+                        sweepAngle = valuePhase - phaseZero,
+                        topLeft = Offset.Zero,
+                        useCenter = false,
+                        size = Size(radius * 2f, radius * 2f),
+                    )
+
+                    // value outline
+                    drawArc(
+                        color = encoderColor,
+                        startAngle = phaseZero,
+                        sweepAngle = valuePhase - phaseZero,
+                        topLeft = Offset.Zero,
+                        useCenter = false,
+                        size = Size(radius * 2f, radius * 2f),
+                        style = Stroke(width = 2.dp.toPx()),
+                    )
+
+                    // triangle to fill from arc filler to center
+                    drawPath(Path().apply {
+                        moveTo(0f, radius)
+                        lineTo(radius, radius)
+                        lineTo(
+                            radius + cos(valuePhaseRadians) * radius,
+                            sin(valuePhaseRadians) * radius + radius
+                        )
+                        close()
+                    }, encoderColorFill)
+
+                    drawLine(
+                        strokeWidth = 2.dp.toPx(),
+                        start = Offset(0f, radius),
+                        end = Offset(radius, radius),
+                        color = encoderColor,
+                    )
+
+                    translate(radius, radius) {
+                        drawLine(
+                            strokeWidth = 2.dp.toPx(),
+                            cap = StrokeCap.Square,
+                            color = encoderColor,
+                            start = Offset.Zero,
+                            end = Offset(
+                                (cos(valuePhaseRadians) * radius),
+                                (sin(valuePhaseRadians) * radius)
+                            )
+                        )
+                    }
+                }
+
+            }
+        }
     }
 }
 
